f08fb2c3a7a6981feb03075a60082ff93650a2c8
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-17 13:19:21 +0500

    Added theme toggler

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { Hero } from "@/components/Hero";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <Hero />
-      </div>
+        <ModeToggle />
     </main>
   );
 }
