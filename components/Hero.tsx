11efd49b17aef75eb4bc55415134db2b79c2f22b
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-20 15:45:20 +0500

    added &apos; to avoid build error

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -16,7 +16,7 @@ export const Hero = () => {
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden bg-background ">
       <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
-        Hi, I'm Zaeem
+        Hi, I&apos;m Zaeem
       </span>
       <Particles
         className="absolute inset-0"
