0b2a1a4ecb3dfec042e9fb27c2e4cf8d9188dd60
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-20 17:32:52 +0500

    Modified Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export const Hero = () => {
 
   return (
     <div className="relative flex h-[500px] w-full flex-col items-center justify-center overflow-hidden bg-background ">
-      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold leading-none text-transparent dark:from-white dark:to-slate-900/10">
+      <span className="pointer-events-none whitespace-pre-wrap bg-gradient-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl font-semibold tracking-tighter leading-none text-transparent dark:from-white dark:to-slate-900/10">
         Hi, I&apos;m Zaeem
       </span>
       <Particles
