f4f73202b00944f43a139a542b70c62b57323815
Muhammad Zaeem Tauqir <zaeemtauqir@hotmail.com>
2024-07-30 11:10:38 +0500

    Added metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Navbar } from "@/components/Navbar";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>My Portfolio</title>
+        <meta name="description" content="Welcome to my portfolio website. Here you can find my projects and contact information." />
+        <meta property="og:title" content="Zaeem Tauqir - Portfolio" />
+        <meta property="og:description" content="Welcome to my portfolio website. Here you can find my projects and contact information." />
+        <meta property="og:image" content="../public/portfolio-preview.png" />
+        <meta property="og:url" content="https://zaeemtauqir.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Zaeem Tauqir - Portfolio" />
+        <meta name="twitter:description" content="Welcome to my portfolio website. Here you can find my projects and contact information." />
+        <meta name="twitter:image" content="../public/portfolio-preview.png" />
+      </Head>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
